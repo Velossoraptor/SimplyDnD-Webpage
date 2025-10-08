481a5e733c5707f22d68423e8776fa46a56f4075
velossoraptor <taliamolsen@gmail.com>
2025-10-08 13:45:52 -0400

    Encounter RNG works, and you can add monsters manually to the list. Also removed Hero image div since it is not used

--- js/dndApi.js
+++ js/dndApi.js
@@ -1,31 +1,14 @@
 const url = "https://www.dnd5eapi.co/api/2014/";
 
-// async function getData(category, query = "") {
-//   try {
-//     const lowerQuery = query.toLowerCase().replace(" ", "-");
-//     const response = await fetch(url + category + "/" + lowerQuery);
-//     if (response.ok) {
-//       const data = await response.json();
-//       return data;
-//     } else if (response.status === 404) {
-//       return `No results found for ${category}/${lowerQuery} - what you're looking for might not exist.`;
-//     } else if (response.status === 500) {
-//       return `Server error - try again later.`;
-//     } else {
-//       throw Error(await response.text());
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
+// GET FUNCTIONS
 
 // Gets name of all the data in the selected category
-export async function getDataGeneral(category, displayResults) {
+export async function getDataGeneral(category, displayResults, context = "bookmark") {
   try {
     const response = await fetch(url + category);
     if (response.ok) {
       const data = await response.json();
-      displayDataGeneral(data.results, category, displayResults);
+      displayDataGeneral(data.results, category, displayResults, context);
     } else if (response.status === 404) {
       displayResults.innerHTML = `<p>No results found for ${category} - what you're looking for might not exist.</p>`;
     } else if (response.status === 500) {
@@ -38,41 +21,14 @@ export async function getDataGeneral(category, displayResults) {
   }
 }
 
-// Displays the name of all the data in the selected category and adds a toggle to show details or back to basics
-function displayDataGeneral(results, category, displayResults) {
-  displayResults.innerHTML = "";
-
-  results.forEach((item) => {
-    const card = document.createElement("div");
-    card.innerHTML = `<div class="monster-card">
-              <p>${item.name}</p>
-              <button class="bookmark-button"></button>
-            </div>`;
-    displayResults.appendChild(card);
-
-    card.addEventListener("click", async (e) => {
-      console.log(item.name + "expanded details");
-      if (card.classList.contains("expanded")) {
-        card.innerHTML = `
-              <p>${item.name}</p>
-              <button class="bookmark-button"></button>
-            `;
-      } else {
-        await getDataDetails(category, item.index, card);
-      }
-      card.classList.toggle("expanded");
-    });
-  });
-}
-
 // Gets the details of the selected item in the selected category
-export async function getDataDetails(category, query, displayCard) {
+export async function getDataDetails(category, query, displayCard, context = "bookmark") {
   try {
     const lowerQuery = query.toLowerCase().replace(" ", "-");
     const response = await fetch(url + category + "/" + lowerQuery);
     if (response.ok) {
       const data = await response.json();
-      displayDataDetails(data, category, displayCard);
+      displayDataDetails(data, category, displayCard, context);
     } else if (response.status === 404) {
       displayCard.innerHTML = `<p>No results found for ${category}/${lowerQuery} - what you're looking for might not exist.</p>`;
     } else if (response.status === 500) {
@@ -85,9 +41,98 @@ export async function getDataDetails(category, query, displayCard) {
   }
 }
 
+// Gets the details of items that fit the query in the selected category
+export async function getDataByQuery(category, query, displayResults, context = "bookmark") {
+  try {
+    const lowerQuery = query.toLowerCase().replace(" ", "-");
+    const response = await fetch(url + category);
+    if (response.ok) {
+      const data = await response.json();
+      const results = data.results;
+      const matchingResults = results.filter((item) =>
+        item.index.includes(lowerQuery),
+      );
+
+      if (matchingResults.length === 0) {
+        displayResults.innerHTML = `<p>No matches found.</p>`;
+        return;
+      }
+
+      displayDataGeneral(matchingResults, category, displayResults, context);
+    } else if (response.status === 404) {
+      displayResults.innerHTML = `<p>No results found for ${category}/${lowerQuery} - what you're looking for might not exist.</p>`;
+    } else if (response.status === 500) {
+      displayResults.innerHTML = `<p>Server error - try again later.</p>`;
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getRandomMonsters(number, displayResults, context = "bookmark", category = "monsters"){
+  try {
+    const response = await fetch(url + category);
+    if(response.ok){
+      const data = await response.json();
+      const results = data.results;
+      const randomResults = [];
+      for(let i = 0; i<number; i++){
+        const randomIndex = Math.floor(Math.random() * results.length);
+        randomResults.push(results[randomIndex]);
+        displayDataGeneral(randomResults, category, displayResults, context);
+      }
+    }
+  }catch (error){
+    console.log(error);
+  }
+}
+
+// DISPLAY FUNCTIONS
+
+// Displays the name of all the data in the selected category and adds a toggle to show details or back to basics
+function displayDataGeneral(results, category, displayResults, context) {
+  displayResults.innerHTML = "";
+  let buttonContent = "";
+  if(context === "add-list"){
+    buttonContent = "+";
+  }
+  let buttonHTML = `<button class="${context}-button">${buttonContent}</button>`;
+
+  results.forEach((item) => {
+    const card = document.createElement("div");
+    card.innerHTML = `<div class="monster-card">
+              <p>${item.name}</p>
+              ${buttonHTML}
+            </div>`;
+    displayResults.appendChild(card);
+
+    card.addEventListener("click", async (e) => {
+      if (card.classList.contains("expanded")) {
+        card.innerHTML = `
+              <p>${item.name}</p>
+              ${buttonHTML}
+            `;
+      } else {
+        await getDataDetails(category, item.index, card, context);
+      }
+      card.classList.toggle("expanded");
+    });
+  });
+}
+
+
+
 // Displays the details of the selected item in the selected category
-function displayDataDetails(data, category, displayCard) {
+function displayDataDetails(data, category, displayCard, context) {
   displayCard.innerHTML = "";
+  let buttonContent = "";
+  if(context === "add-list"){
+    buttonContent = "+";
+  }
+  let buttonHTML = `<button class="${context}-button">${buttonContent}</button>`;
+
   if (category === "monsters") {
     displayCard.innerHTML = `
     <div>
@@ -95,7 +140,7 @@ function displayDataDetails(data, category, displayCard) {
         <p>${data.size} ${data.type} | CR ${data.challenge_rating} | AC ${data.armor_class.value} | XP ${data.xp}</p>
         <p>HP: ${data.hit_points} | Speed: Walk ${data.speed.walk}</p>
     <div>
-    <button class="bookmark-button"></button>
+    ${buttonHTML}
     `;
   } else if (category === "spells") {
     let conc = "";
@@ -115,7 +160,7 @@ function displayDataDetails(data, category, displayCard) {
         <p>${attack} ${data.school.name} | Level ${data.level} | Duration ${data.duration} | C ${conc}</p>
         <p>"${data.desc}"</p>
     </div>
-    <button class="bookmark-button"></button>
+    ${buttonHTML}
     `;
   } else if (category === "features") {
     displayCard.innerHTML = `
@@ -124,7 +169,7 @@ function displayDataDetails(data, category, displayCard) {
         <p>${data.class.name} | Level ${data.level}</p>
         <p>"${data.desc}"</p>
     </div>
-    <button class="bookmark-button"></button>
+    ${buttonHTML}
     `;
   } else if (category === "races") {
     const details = [
@@ -141,7 +186,7 @@ function displayDataDetails(data, category, displayCard) {
         <p>Speed ${data.speed} | ${data.size}</p>
         <p>"${details.join("<br><br>")}"</p>
     </div>
-    <button class="bookmark-button"></button>
+    ${buttonHTML}
     `;
   } else if (category === "classes") {
     displayCard.innerHTML = `
@@ -151,38 +196,10 @@ function displayDataDetails(data, category, displayCard) {
         <p>Proficiencies: ${data.proficiency_choices[0].desc}</p>
         <p>Starting Equipment: ${data.starting_equipment_options[0].desc}</p>
     </div>
-    <button class="bookmark-button"></button>
+    ${buttonHTML}
     `;
   }
   displayCard.classList.add("monster-card");
 }
 
-// Gets the details of items that fit the query in the selected category
-export async function getDataByQuery(category, query, displayResults) {
-  try {
-    const lowerQuery = query.toLowerCase().replace(" ", "-");
-    const response = await fetch(url + category);
-    if (response.ok) {
-      const data = await response.json();
-      const results = data.results;
-      const matchingResults = results.filter((item) =>
-        item.index.includes(lowerQuery),
-      );
 
-      if (matchingResults.length === 0) {
-        displayResults.innerHTML = `<p>No matches found.</p>`;
-        return;
-      }
-
-      displayDataGeneral(matchingResults, category, displayResults);
-    } else if (response.status === 404) {
-      displayResults.innerHTML = `<p>No results found for ${category}/${lowerQuery} - what you're looking for might not exist.</p>`;
-    } else if (response.status === 500) {
-      displayResults.innerHTML = `<p>Server error - try again later.</p>`;
-    } else {
-      throw Error(await response.text());
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
--- js/encounter.js
+++ js/encounter.js
@@ -1,9 +1,15 @@
-import { getDataGeneral, getDataByQuery } from "./dndApi.js";
+import { getDataGeneral, getDataByQuery, getRandomMonsters } from "./dndApi.js";
 
 const displayResults = document.querySelector(".results");
 const searchButton = document.querySelector(".search-button");
 const searchInput = document.getElementById("search");
+const context = "add-list";
+const encounterList = document.querySelector("#encounter-list");
+const clearButton = document.getElementById("clear");
+const randomButton = document.getElementById("random-encounter");
+const form = document.querySelector("form");
 
+// Listens for input in the search bar and fetches based on data
 searchInput.addEventListener("input", async (e) => {
   if (e.key === "Enter") {
     e.preventDefault();
@@ -11,14 +17,53 @@ searchInput.addEventListener("input", async (e) => {
   const category = document.getElementById("category").value;
   const query = searchInput.value;
   if (query != null && query != "") {
-    getDataByQuery(category, query, displayResults);
+    getDataByQuery(category, query, displayResults, context);
   } else {
-    getDataGeneral(category, displayResults);
+    getDataGeneral(category, displayResults, context);
   }
 });
 
+// Listens for search button click and fetches based on data
 searchButton.addEventListener("click", async (e) => {
   e.preventDefault();
   const category = document.getElementById("category").value;
-  await getDataGeneral(category, displayResults);
+  await getDataGeneral(category, displayResults, context);
+});
+
+
+// Listens for add-to-list button click
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("add-list-button")) {
+    addToList(e.target);
+  }
+});
+
+// Adds monster to encounter list
+function addToList(button) {
+  if (encounterList.querySelector("#default-message")) {
+    encounterList.querySelector("#default-message").remove();
+  }
+  let name;
+  if (button.parentElement.parentElement.classList.contains("expanded")) {
+    name = button.parentElement.querySelector("p").innerText;
+  } else {
+    name = button.parentElement.parentElement.querySelector("p").innerText;
+  }
+
+  const monsterCard = document.createElement("div");
+  monsterCard.classList.add("monster-card");
+  const outerCard = document.createElement("div");
+  monsterCard.innerHTML = name;
+  outerCard.appendChild(monsterCard);
+  encounterList.appendChild(outerCard);
+}
+
+// Clears encounter list
+clearButton.addEventListener("click", (e)=>{
+  encounterList.innerHTML = '<p id="default-message">Your encounter will appear here!</p>';
+});
+
+randomButton.addEventListener("click", async (e)=>{
+  const number = form.elements["num-mon"].value;
+  await getRandomMonsters(number, encounterList, context);
 });
